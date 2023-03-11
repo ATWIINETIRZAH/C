2ab0f1d0da3e7e7e31f57efeba1afeb00fb26fc2
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-03-11 23:56:22 +0300

    Add files via upload
    finding the ascii value of any character

---
+++ ASCII.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+    printf("\n\nASCII value of %c = %d",c,c);
+
+
+    return 0;
+}
